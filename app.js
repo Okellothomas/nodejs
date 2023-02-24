a70fafaf7d0d68c05eac3340261396f82051d59d
Okellothomas <thomasolal33@gmail.com>
2023-02-24 21:25:53 +0300

    add initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/**
+ * This is the first projects
+ */
+
+const costs = 30
+
+if (costs < 21) {
+    console.log(`This is the one we should call for ${costs}`)
+} else {
+    console.log(`This is not the one we should do ${costs}`)
+}
+
+console.log(`this is the one we should do come one ${costs}`)
