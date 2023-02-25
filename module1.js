f0c1749169878ce324ae8a30205adff7adcd14cb
Okellothomas <thomasolal33@gmail.com>
2023-02-25 15:51:02 +0300

    save models one

---
+++ module1.js
@@ -0,0 +1,14 @@
+/**
+ * module
+ */
+
+let john = "John"
+let thomas = "Thomas"
+
+const Surname = (name) => {
+    console.log(`Hello ${name} how are you doing brother!`)
+}
+
+Surname("Susana")
+Surname(john)
+Surname(thomas)
---
+++ sum.js
@@ -0,0 +1,15 @@
+/**
+ * The next node project
+ */
+
+function sum(num1, num2) {
+    let sum = num1 + num2
+
+    if (sum < 30) {
+        console.log("Credit!")
+    } else {
+        console.log("Fail!")
+    }
+}
+
+sum(10, 30)
