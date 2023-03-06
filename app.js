ce701e3667947490a2b39c22bed43e9a0709e948
Okellothomas <thomasolal33@gmail.com>
2023-03-06 08:20:51 +0300

    last commit in the morning

--- app.js
+++ app.js
@@ -2,22 +2,31 @@
  * This is the first projects
  */
 
-const costs = 30
-const EventEmitter = require('events')
+// const costs = 30
+// const EventEmitter = require('events')
 
-const customEmitter = new EventEmitter()
+// const customEmitter = new EventEmitter()
 
 
-customEmitter.on('response', () => {
-    console.log('data received')
+// customEmitter.on('response', () => {
+//     console.log('data received')
+// })
+
+// customEmitter.on('response', () => {
+//     console.log('some other logic here!..')
+// })
+
+// customEmitter.on('response', () => {
+//     console.log('With these things we should!..')
+// })
+
+// customEmitter.emit('response')
+
+const http = require('http')
+
+
+const server = http.createServer((req, res) => {
+    res.end('Home Page Nigaa')
 })
 
-customEmitter.on('response', () => {
-    console.log('some other logic here!..')
-})
-
-customEmitter.on('response', () => {
-    console.log('With these things we should!..')
-})
-
-customEmitter.emit('response')
+server.listen(4000)
---
+++ http-stream.js
@@ -0,0 +1,8 @@
+var http = require('http')
+var fs = require('fs')
+
+http.createServer((req, res) => {
+    const text = fs.readFileSync('./content/big.txt', 'utf8')
+    res.end(text)
+})
+.listen(3000)
